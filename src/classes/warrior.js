31b79902446983b6e22547ef954abadb01de656e
Marcus Nguyen <dmnguyen1996@gmail.com>
2020-05-24 13:55:43 +1200

    Warrior now taunts monster that is targeting other party members

--- src/classes/warrior.js
+++ src/classes/warrior.js
@@ -1,7 +1,13 @@
 import default_melee_attack from './default_melee_attack'
 
+const useTaunt = (target) => {
+  const skill = G.skills['taunt']
+  if (!is_on_cooldown('taunt') && character.mp > skill.mp) use_skill('taunt', target)
+}
+
 export default {
   attack(target) {
+    if (get_target_of(target) !== character.name) useTaunt(target)
     default_melee_attack(target)
   },
 }
